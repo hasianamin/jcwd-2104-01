8fd08dc5d77fa38dbe81a242976d6cd6210527e0
hasianamin <hasianamin14@gmail.com>
2022-09-08 16:54:59 +0700

    test

--- deploy.config.js
+++ deploy.config.js
@@ -4,7 +4,6 @@ module.exports = {
       name: "JCWD-2104-01", // Format JCWD-{batchcode}-{groupnumber}
       script: "./projects/server/src/index.js",
       env: {
-        NODE_ENV: "production",
         PORT: 8401,
       },
       time: true,
